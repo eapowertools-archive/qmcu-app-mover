d1ac90af91881796d9a9ec236c6d599503a45bba
Jesse Paris <jparis@gmail.com>
2016-11-03 16:44:10 -0700

    now deploying cross platform

--- copyNPMPackages.bat
+++ copyNPMPackages.bat
@@ -1,1 +1,9 @@
-xcopy appMover\node_modules ..\QlikSenseQMCUtility\plugins\appMover\node_modules /I /Y /R /S
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    rsync -r ./appMover/node_modules ../QlikSenseQMCUtility/plugins/appMover
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Linux not supported."
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    xcopy appMover\node_modules ..\QlikSenseQMCUtility\plugins\appMover\node_modules /I /Y /R /S
+fi
--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    echo "hello world"
+    rsync -r ./appMover ../QlikSenseQMCUtility/plugins/ --exclude */node_modules/
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Linux not supported."
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
