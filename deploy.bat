bdbefe32adff1934db57b608f73503a3f2dbcf4a
Jesse Paris <jparis@gmail.com>
2016-11-03 14:15:49 -0700

    hello world for osx

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,9 @@
-xcopy appMover ..\QlikSenseQMCUtility\plugins\appMover /I /Y /R /S /EXCLUDE:exclusionList.txt
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "hello world"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Linux not supported."
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    xcopy appMover ..\QlikSenseQMCUtility\plugins\appMover /I /Y /R /S /EXCLUDE:exclusionList.txt
+fi
