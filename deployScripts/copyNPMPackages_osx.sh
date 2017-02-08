8b4d07944143a3ca0c1d699b34555ba1eb74d857
Jesse Paris <Jesse.Paris@qlik.com>
2017-02-08 09:13:31 -0800

    added deploy files for osx and windows.

--- deployScripts/copyNPMPackages_osx.sh
+++ deployScripts/copyNPMPackages_osx.sh
@@ -4,6 +4,4 @@ if [ "$(uname)" == "Darwin" ]; then
     rsync -r ./appMover/node_modules ../QlikSenseQMCUtility/plugins/appMover
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Linux not supported."
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    xcopy appMover\node_modules ..\QlikSenseQMCUtility\plugins\appMover\node_modules /I /Y /R /S
 fi
